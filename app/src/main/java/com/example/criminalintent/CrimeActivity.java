0e7c012f16a544a6b7311589ba9024fe4f09b57d
zzq <zhouzhuoqun163@163.com>
2020-04-25 17:22:09 +0800

    第七章 UI fragment 与 fragment 管理器

---
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -0,0 +1,26 @@
+package com.example.criminalintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment==null){
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
